2855191518af2fef334b1cddff44ec00268e8061
lena0911 <lenagolovkina09@gmail.com>
2022-04-09 19:30:53 +0300

    Задание по task-inheritance

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -16,6 +16,20 @@ export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+
+    constructor(param: IPersonData) {
+        this._age = param.age;
+        this._name = param.name;
+        this._secondName = param.secondName;
+    }
+
+    getData(): IPersonData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
 
 /*
@@ -24,4 +38,18 @@ export class Person {
  */
 export class Student extends Person {
     readonly _phone: string;
+
+    constructor(param: IStudentData) {
+        super(param);
+        this._phone = param.phone;
+    }
+
+    getData(): IStudentData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+            phone: this._phone,
+        };
+    }
 }
